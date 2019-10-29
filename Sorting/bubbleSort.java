7af6f2a28f984fa7176bd4aa9a9a20d740cb59f1
Noah Park <noahjinpark@gmail.com>
2019-10-29 11:26:18 -0500

    Create bubbleSort.java

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,50 @@
+// CTCI Bubble Sort Problem
+// Github: github.com/noahjpark
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    static void countSwaps(int[] a) {
+        int numSwaps = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    a = swap(a, j, j + 1);
+                    numSwaps++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+
+    static int[] swap(int[] a, int b, int c){
+        int temp = a[b];
+        a[b] = a[c];
+        a[c] = temp;
+        return a;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int[] a = new int[n];
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+        countSwaps(a);
+        scanner.close();
+    }
+}
